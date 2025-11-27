05fa7132e26d28f4e8e9cefdb5ca4628c1501695
Adonias-hibeste <adoniashibestegithub@gmail.com>
2025-11-27 17:26:12 +0300

    fix: add external image domains to Next.js config
    - Configure remotePatterns for Imgur, ImgBB, Cloudinary
    - Allow Vercel Blob Storage images
    - Fix project images not displaying from external URLs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,30 @@ const nextConfig = {
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
   transpilePackages: ["@react-pdf/renderer"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.vercel-storage.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
